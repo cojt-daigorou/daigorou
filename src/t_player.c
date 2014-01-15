0a5709b25c4701c3325331ce170b259be4e13e1e
Sho Hosoda <gam0022@gmail.com>
2014-01-15 14:59:26 +0900

    落下の速度を 8→15 に上げた。インデントの調整。

--- src/t_player.c
+++ src/t_player.c
@@ -184,264 +184,264 @@ static s32 CalcPlayer( struct TaskData* pTask , u32 Flag ) {
     case PLAYER_MODE_WALK :
     case PLAYER_MODE_WALKEND :
       isRun = (PadLvl()&PAD_B) == PAD_B;
-			if( g_PlayerY < GROUND_LINE && !MovePlayer( pTask , 0 , 10 , 0 )) {
-					//　一番下でなく、足場が無くなった場合は下降モードへ。
-				g_PlayerY += BBox[ PLAYER_MODE_WAIT ].y1 - BBox[ PLAYER_MODE_FALL ].y1;
+      if( g_PlayerY < GROUND_LINE && !MovePlayer( pTask , 0 , 10 , 0 )) {
+        //　一番下でなく、足場が無くなった場合は下降モードへ。
+        g_PlayerY += BBox[ PLAYER_MODE_WAIT ].y1 - BBox[ PLAYER_MODE_FALL ].y1;
 
-				pTask->Data.player.mode = PLAYER_MODE_FALL;
-				pTask->Data.player.count = 0;
-			}
-			else if( PadTrg()&PAD_X ) {
-				pTask->Data.player.mode = PLAYER_MODE_ATTACK;
-				pTask->Data.player.count = 0;
-			}
-			else if( PadTrg()&PAD_A ) {		//　ボタンが押された場合はジャンプモードへ。
-				pTask->Data.player.mode = PLAYER_MODE_JUMPSTART;
-				pTask->Data.player.count = 0;
-				ageSndMgrPlayOneshot( AS_SND_JUMP , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
-			}
-			else if( PadLvl()&PAD_RIGHT || PadLvl()&PAD_LEFT ) {
-				if( PadLvl()&PAD_RIGHT ) {
-					pTask->Data.player.direction = 0;
-					MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
-				}
-				else {
-					pTask->Data.player.direction = 1;
-					MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
-				};
+        pTask->Data.player.mode = PLAYER_MODE_FALL;
+        pTask->Data.player.count = 0;
+      }
+      else if( PadTrg()&PAD_X ) {
+        pTask->Data.player.mode = PLAYER_MODE_ATTACK;
+        pTask->Data.player.count = 0;
+      }
+      else if( PadTrg()&PAD_A ) {		//　ボタンが押された場合はジャンプモードへ。
+        pTask->Data.player.mode = PLAYER_MODE_JUMPSTART;
+        pTask->Data.player.count = 0;
+        ageSndMgrPlayOneshot( AS_SND_JUMP , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
+      }
+      else if( PadLvl()&PAD_RIGHT || PadLvl()&PAD_LEFT ) {
+        if( PadLvl()&PAD_RIGHT ) {
+          pTask->Data.player.direction = 0;
+          MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
+        }
+        else {
+          pTask->Data.player.direction = 1;
+          MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
+        };
 
-				if( pTask->Data.player.mode == PLAYER_MODE_WAIT ) {		//　止まってたら走り始める
-					pTask->Data.player.mode = PLAYER_MODE_WALKSTART;
-					pTask->Data.player.count = 0;
-				}
-				else if( pTask->Data.player.mode == PLAYER_MODE_WALKEND ) {		//　止まりかけだったなら走る
-					pTask->Data.player.mode = PLAYER_MODE_WALK;
-					pTask->Data.player.count = 0;
-				}
-				else {
-					pTask->Data.player.count++;
+        if( pTask->Data.player.mode == PLAYER_MODE_WAIT ) {		//　止まってたら走り始める
+          pTask->Data.player.mode = PLAYER_MODE_WALKSTART;
+          pTask->Data.player.count = 0;
+        }
+        else if( pTask->Data.player.mode == PLAYER_MODE_WALKEND ) {		//　止まりかけだったなら走る
+          pTask->Data.player.mode = PLAYER_MODE_WALK;
+          pTask->Data.player.count = 0;
+        }
+        else {
+          pTask->Data.player.count++;
 
-					if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode + isRun*8 ] ].Frames ) {
-						pTask->Data.player.count = 0;
+          if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode + isRun*8 ] ].Frames ) {
+            pTask->Data.player.count = 0;
 
-						if( pTask->Data.player.mode == PLAYER_MODE_WALKSTART ) {		//　走り始めが終わったら走りモードへ。
-							pTask->Data.player.mode = PLAYER_MODE_WALK;
-						};
-					};
-				};
+            if( pTask->Data.player.mode == PLAYER_MODE_WALKSTART ) {		//　走り始めが終わったら走りモードへ。
+              pTask->Data.player.mode = PLAYER_MODE_WALK;
+            };
+          };
+        };
 
-				if( (pTask->Data.player.count & 0x1f) == 0 ) {
-					ageSndMgrPlayOneshot( AS_SND_WALK_RIGHT , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
-				}
-				else if( (pTask->Data.player.count & 0x1f) == 0x0f ) {
-					ageSndMgrPlayOneshot( AS_SND_WALK_LEFT , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
-				};
-			}
-			else {		//　キー入力なし
-				if( pTask->Data.player.mode == PLAYER_MODE_WALKSTART ) {		//　走り始めは停止へ。
-					pTask->Data.player.mode = PLAYER_MODE_WAIT;
-					pTask->Data.player.count = 0;
-				}
-				else if( pTask->Data.player.mode == PLAYER_MODE_WALK ) {		//　走ってたら走り終わりへ。
-					pTask->Data.player.mode = PLAYER_MODE_WALKEND;
-					pTask->Data.player.count = 0;
+        if( (pTask->Data.player.count & 0x1f) == 0 ) {
+          ageSndMgrPlayOneshot( AS_SND_WALK_RIGHT , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
+        }
+        else if( (pTask->Data.player.count & 0x1f) == 0x0f ) {
+          ageSndMgrPlayOneshot( AS_SND_WALK_LEFT , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
+        };
+      }
+      else {		//　キー入力なし
+        if( pTask->Data.player.mode == PLAYER_MODE_WALKSTART ) {		//　走り始めは停止へ。
+          pTask->Data.player.mode = PLAYER_MODE_WAIT;
+          pTask->Data.player.count = 0;
+        }
+        else if( pTask->Data.player.mode == PLAYER_MODE_WALK ) {		//　走ってたら走り終わりへ。
+          pTask->Data.player.mode = PLAYER_MODE_WALKEND;
+          pTask->Data.player.count = 0;
 
-				}
-				else {		//　止まってるか走り終わり
-					pTask->Data.player.count++;
+        }
+        else {		//　止まってるか走り終わり
+          pTask->Data.player.count++;
 
-					if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode + isRun*8 ] ].Frames ) {
-						pTask->Data.player.count = 0;
+          if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode + isRun*8 ] ].Frames ) {
+            pTask->Data.player.count = 0;
 
-						if( pTask->Data.player.mode == PLAYER_MODE_WALKEND ) {
-							pTask->Data.player.mode = PLAYER_MODE_WAIT;
-						};
-					};
-				};
-			};
-			break;
+            if( pTask->Data.player.mode == PLAYER_MODE_WALKEND ) {
+              pTask->Data.player.mode = PLAYER_MODE_WAIT;
+            };
+          };
+        };
+      };
+      break;
 
-		case PLAYER_MODE_JUMPSTART :
-			if( PadLvl()&PAD_RIGHT ) {
-				pTask->Data.player.direction = 0;
-				MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
-			};
-			if( PadLvl()&PAD_LEFT ) {
-				pTask->Data.player.direction = 1;
-				MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
-			};
+    case PLAYER_MODE_JUMPSTART :
+      if( PadLvl()&PAD_RIGHT ) {
+        pTask->Data.player.direction = 0;
+        MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
+      };
+      if( PadLvl()&PAD_LEFT ) {
+        pTask->Data.player.direction = 1;
+        MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
+      };
 
-			/*if( PadLvl()&PAD_A ) {*/		//　ボタンが押されている場合 <s01151244>Aを離しても停止しないように
-				pTask->Data.player.count++;
+      /*if( PadLvl()&PAD_A ) {*/		//　ボタンが押されている場合 <s01151244>Aを離しても停止しないように
+      pTask->Data.player.count++;
 
-				if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
-					pTask->Data.player.count = 0;
-					pTask->Data.player.mode = PLAYER_MODE_JUMP;
-					pTask->Data.player.jump_count = 0;
-				};
-			/*}
-			else {		//　ボタンが離された（ジャンプ直前なので停止へ移行）
-				pTask->Data.player.mode = PLAYER_MODE_WAIT;
-				pTask->Data.player.count = 0;
-			};</s01151244>*/
-			break;
+      if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
+        pTask->Data.player.count = 0;
+        pTask->Data.player.mode = PLAYER_MODE_JUMP;
+        pTask->Data.player.jump_count = 0;
+      };
+      /*}
+        else {		//　ボタンが離された（ジャンプ直前なので停止へ移行）
+        pTask->Data.player.mode = PLAYER_MODE_WAIT;
+        pTask->Data.player.count = 0;
+        };</s01151244>*/
+      break;
 
-		case PLAYER_MODE_JUMP :
-			if( PadLvl()&PAD_RIGHT ) {
-				pTask->Data.player.direction = 0;
-				MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
-			};
-			if( PadLvl()&PAD_LEFT ) {
-				pTask->Data.player.direction = 1;
-				MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
-			};
+    case PLAYER_MODE_JUMP :
+      if( PadLvl()&PAD_RIGHT ) {
+        pTask->Data.player.direction = 0;
+        MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
+      };
+      if( PadLvl()&PAD_LEFT ) {
+        pTask->Data.player.direction = 1;
+        MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
+      };
 
-			if( PadLvl()&PAD_A ||(pTask->Data.player.jump_count<10)) {		/*<s01151244>ボタンが押されている場合orジャンプカウントが10になるまで*/
-				MovePlayer( pTask , 0 , -JumpPattern[ pTask->Data.player.jump_count ] , 1 );
-				pTask->Data.player.jump_count++;
+      if( PadLvl()&PAD_A ||(pTask->Data.player.jump_count<10)) {		/*<s01151244>ボタンが押されている場合orジャンプカウントが10になるまで*/
+        MovePlayer( pTask , 0 , -JumpPattern[ pTask->Data.player.jump_count ] , 1 );
+        pTask->Data.player.jump_count++;
 
-				if( pTask->Data.player.jump_count >= sizeof( JumpPattern )/sizeof( JumpPattern[0] ) ) {
-					pTask->Data.player.mode = PLAYER_MODE_FALL;
-				};
+        if( pTask->Data.player.jump_count >= sizeof( JumpPattern )/sizeof( JumpPattern[0] ) ) {
+          pTask->Data.player.mode = PLAYER_MODE_FALL;
+        };
 
-				pTask->Data.player.count++;
-				if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
-					pTask->Data.player.count = 0;
-				};
-			}
-			else {		//　ボタンが離されたので落ちモードへ。
-				pTask->Data.player.mode = PLAYER_MODE_FALL;
-			};
-			break;
+        pTask->Data.player.count++;
+        if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
+          pTask->Data.player.count = 0;
+        };
+      }
+      else {		//　ボタンが離されたので落ちモードへ。
+        pTask->Data.player.mode = PLAYER_MODE_FALL;
+      };
+      break;
 
-		case PLAYER_MODE_FALL :
-			if( PadLvl()&PAD_RIGHT ) {
-				pTask->Data.player.direction = 0;
-				MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
-			};
-			if( PadLvl()&PAD_LEFT ) {
-				pTask->Data.player.direction = 1;
-				MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
-			};
+    case PLAYER_MODE_FALL :
+      if( PadLvl()&PAD_RIGHT ) {
+        pTask->Data.player.direction = 0;
+        MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
+      };
+      if( PadLvl()&PAD_LEFT ) {
+        pTask->Data.player.direction = 1;
+        MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
+      };
 
-			if( MovePlayer( pTask , 0 , 8 , 1 ) ) {
-				g_PlayerY -= BBox[ PLAYER_MODE_WAIT ].y1 - BBox[ PLAYER_MODE_FALL ].y1;
+      if( MovePlayer( pTask , 0 , 15 , 1 ) ) {
+        g_PlayerY -= BBox[ PLAYER_MODE_WAIT ].y1 - BBox[ PLAYER_MODE_FALL ].y1;
 
-				pTask->Data.player.mode = PLAYER_MODE_JUMPEND;
-				pTask->Data.player.count = 0;
-			}
-			else if( g_PlayerY >= GROUND_LINE ) {
-				g_PlayerY = GROUND_LINE;
-				pTask->Data.player.mode = PLAYER_MODE_JUMPEND;
-				pTask->Data.player.count = 0;
-			}
-			else {
-				pTask->Data.player.count++;
+        pTask->Data.player.mode = PLAYER_MODE_JUMPEND;
+        pTask->Data.player.count = 0;
+      }
+      else if( g_PlayerY >= GROUND_LINE ) {
+        g_PlayerY = GROUND_LINE;
+        pTask->Data.player.mode = PLAYER_MODE_JUMPEND;
+        pTask->Data.player.count = 0;
+      }
+      else {
+        pTask->Data.player.count++;
 
-				if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
-					pTask->Data.player.count = 0;
-				};
-			};
-			break;
+        if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
+          pTask->Data.player.count = 0;
+        };
+      };
+      break;
 
-		case PLAYER_MODE_JUMPEND :
-			if( PadLvl()&PAD_RIGHT ) {
-				pTask->Data.player.direction = 0;
-				MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
-			};
-			if( PadLvl()&PAD_LEFT ) {
-				pTask->Data.player.direction = 1;
-				MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
-			};
+    case PLAYER_MODE_JUMPEND :
+      if( PadLvl()&PAD_RIGHT ) {
+        pTask->Data.player.direction = 0;
+        MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
+      };
+      if( PadLvl()&PAD_LEFT ) {
+        pTask->Data.player.direction = 1;
+        MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
+      };
 
-			pTask->Data.player.count++;
+      pTask->Data.player.count++;
 
-			if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
-				pTask->Data.player.count = 0;
-				pTask->Data.player.mode = PLAYER_MODE_WAIT;
-				ageSndMgrPlayOneshot( AS_SND_LANDING , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
-			};
-			break;
+      if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
+        pTask->Data.player.count = 0;
+        pTask->Data.player.mode = PLAYER_MODE_WAIT;
+        ageSndMgrPlayOneshot( AS_SND_LANDING , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
+      };
+      break;
 
-		case PLAYER_MODE_ATTACK :
+    case PLAYER_MODE_ATTACK :
 
-			/*<s01151238>攻撃中の左右移動*/
-			if( PadLvl()&PAD_RIGHT ) {
-				pTask->Data.player.direction = 0;
-				MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
-			};
-			if( PadLvl()&PAD_LEFT ) {
-				pTask->Data.player.direction = 1;
-				MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
-			};
-			/*</s0115>*バグあり*/
-			 {
-							struct TaskData* pBTask;
+      /*<s01151238>攻撃中の左右移動*/
+      if( PadLvl()&PAD_RIGHT ) {
+        pTask->Data.player.direction = 0;
+        MovePlayer( pTask , CalcPlayerSpeed() , 0 , 1 );
+      };
+      if( PadLvl()&PAD_LEFT ) {
+        pTask->Data.player.direction = 1;
+        MovePlayer( pTask , -CalcPlayerSpeed() , 0 , 1 );
+      };
+      /*</s0115>*バグあり*/
+      {
+        struct TaskData* pBTask;
 
-							pBTask = AllocTask();
-							InitTaskPBullet( pBTask , pTask->x , pTask->y,1,10,0,0,0 );
-							AddlLink( pBTask , DISP_LEVEL_ENEMY );
-						};
-			pTask->Data.player.count++;
+        pBTask = AllocTask();
+        InitTaskPBullet( pBTask , pTask->x , pTask->y,1,10,0,0,0 );
+        AddlLink( pBTask , DISP_LEVEL_ENEMY );
+      };
+      pTask->Data.player.count++;
 
-			if( pTask->Data.player.count > 10 ) {
-				int x , y;
-				struct TaskData* pFTask;
+      if( pTask->Data.player.count > 10 ) {
+        int x , y;
+        struct TaskData* pFTask;
 
-				pFTask = GetDispLink( DISP_LEVEL_ENEMY );
+        pFTask = GetDispLink( DISP_LEVEL_ENEMY );
 
-				x = g_PlayerX;
-				y = g_PlayerY + BBox[ pTask->Data.player.mode ].y1;
+        x = g_PlayerX;
+        y = g_PlayerY + BBox[ pTask->Data.player.mode ].y1;
 
-				if( pTask->Data.player.direction == 0 ) {
-					x += 120;
-				}
-				else {
-					x -= 120;
-				};
+        if( pTask->Data.player.direction == 0 ) {
+          x += 120;
+        }
+        else {
+          x -= 120;
+        };
 
-				while( pFTask != NULL ) {
-					if( pFTask->type == TASK_ENEMY ) {
-						if( (pFTask->x - x)*(pFTask->x - x) + (pFTask->y - y)*(pFTask->y - y) < 50*50 ) {
-							struct TaskData* pATask;
+        while( pFTask != NULL ) {
+          if( pFTask->type == TASK_ENEMY ) {
+            if( (pFTask->x - x)*(pFTask->x - x) + (pFTask->y - y)*(pFTask->y - y) < 50*50 ) {
+              struct TaskData* pATask;
 
-							pATask = AllocTask();
-							InitTaskAttack( pATask , pFTask->x , pFTask->y );
-							AddlLink( pATask , DISP_LEVEL_ENEMY );
+              pATask = AllocTask();
+              InitTaskAttack( pATask , pFTask->x , pFTask->y );
+              AddlLink( pATask , DISP_LEVEL_ENEMY );
 
-							pFTask->visible = 0;
-							pFTask->flag = TASK_FLAG_DESTROY;
+              pFTask->visible = 0;
+              pFTask->flag = TASK_FLAG_DESTROY;
 
-							AddScore( pFTask->Data.frog.score );
-						};
-					};
+              AddScore( pFTask->Data.frog.score );
+            };
+          };
 
-					pFTask = pFTask->Next;
-				};
-			};
+          pFTask = pFTask->Next;
+        };
+      };
 
-			if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
-				pTask->Data.player.count = 0;
-				pTask->Data.player.mode = PLAYER_MODE_WAIT;
-			};
-			break;
+      if( (pTask->Data.player.count>>1) >= ageRM3[ MotionMap[ pTask->Data.player.mode ] ].Frames ) {
+        pTask->Data.player.count = 0;
+        pTask->Data.player.mode = PLAYER_MODE_WAIT;
+      };
+      break;
 
-		default :
-			break;
-	};
+    default :
+      break;
+  };
 
-			//　アイテム取得
-	if( GetFood( pTask ) ) {
-		ageSndMgrPlayOneshot( AS_SND_GET3 , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
-	};
+  //　アイテム取得
+  if( GetFood( pTask ) ) {
+    ageSndMgrPlayOneshot( AS_SND_GET3 , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
+  };
 
-		//　画面オフセット補正
-	if( g_PlayerX > (1024-256) ) {
-		int nx = g_PlayerX - (1024-256);
-		if( nx > g_OffsetX ) {
-			g_OffsetX = nx;
-		};
-	};
+  //　画面オフセット補正
+  if( g_PlayerX > (1024-256) ) {
+    int nx = g_PlayerX - (1024-256);
+    if( nx > g_OffsetX ) {
+      g_OffsetX = nx;
+    };
+  };
   if ((g_PlayerX - g_OffsetX) < 256) {
     g_OffsetX = g_PlayerX - 256;
     if (g_OffsetX < 0 ) {
