0f1827d0c921b14b424e1920238f90f3ec4f2a3c
Sho Hosoda <gam0022@gmail.com>
2014-01-16 19:52:33 +0900

    Objectにオプションを追加。 - 触れると死亡(未実装) - 破壊可能(未実装) - 振幅 - 周波数

--- src/t_object.h
+++ src/t_object.h
@@ -13,14 +13,20 @@ struct ObjectData {
   u32 count;
 	u16 image;
 	u16 is_hit;
+  u16 is_harmful;
+  u16 is_breakable;
   enum ObjectMotion motion;
   s32 center_x;
   s32 pre_x;
   s32 center_y;
   s32 pre_y;
   float phase;
+  float frequency;
+  float amplitude;
 };
 
-void InitTaskObject( struct TaskData* pTask , s32 x , s32 y , u16 image , u16 is_hit, enum ObjectMotion motion, float phase );
+void InitTaskObject( struct TaskData* pTask, s32 x, s32 y, u16 image,
+    u16 is_hit, u16 is_harmful, u16 is_breakable, 
+    enum ObjectMotion motion, float phase, float frequency, float amplitude );
 
 #endif // __TASK_OBJECT_H__
