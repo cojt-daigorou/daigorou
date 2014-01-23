357678c03b3eb2796ae978ff6e4345751be421bd
Sho Hosoda <gam0022@gmail.com>
2014-01-23 14:18:07 +0900

    ストーリ画面をとりあえず作った。

--- src/t_story.c
+++ src/t_story.c
@@ -13,34 +13,52 @@
 /*                              story                             */
 /******************************************************************/
 static s32 CalcStory( struct TaskData* pTask , u32 Flag ) {
-	if( pTask->Data.story.mode == 0 ) {
-		pTask->Data.story.count++;
 
-		if( PadTrg()&PAD_A || PadTrg()&PAD_B || (PadTrg()&PAD_LEFT && PadTrg()&PAD_RIGHT) ) {
-			pTask->Data.story.mode = 1;
-			pTask->Data.story.count = 0;
+  switch( pTask->Data.story.mode ) {
+    case 0:// message: 目を離したらえりたんがいないのだ!
+    case 1:// message: えりたん!今、会いに行くのだ!
+      pTask->Data.story.count++;
 
-			ageSndMgrPlayOneshot( AS_SND_DECIDE008 , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
-		};
-	}
-	else {
-		pTask->Data.story.count += 64;
+      if( PadTrg()&PAD_A || PadTrg()&PAD_B || (PadTrg()&PAD_LEFT && PadTrg()&PAD_RIGHT) ) {
+        pTask->Data.story.mode++;
+        pTask->Data.story.count = 0;
 
-		ageSndMgrSetVolume( pTask->Data.story.bgm_handle , 0xa0 - pTask->Data.story.count/32 );
-		if( pTask->Data.story.count/32 >= 0xa0 ) {
-			ageSndMgrRelease( pTask->Data.story.bgm_handle );
+        ageSndMgrPlayOneshot( AS_SND_DECIDE008 , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
+      };
+      break;
 
-			GotoMode( MODE_GAME );
-		};
-	};
+    case 2:// スタートボタン後
+      pTask->Data.story.count += 64;
+
+      ageSndMgrSetVolume( pTask->Data.story.bgm_handle , 0xa0 - pTask->Data.story.count/32 );
+      if( pTask->Data.story.count/32 >= 0xa0 ) {
+        ageSndMgrRelease( pTask->Data.story.bgm_handle );
+
+        GotoMode( MODE_GAME );
+      };
+      break;
+  };
 
 	return( 0 );
+}
+
+static s32 max(int a, int b) {
+  return a > b ? a : b;
+}
+
+static s32 min(int a, int b) {
+  return a < b ? a : b;
+}
+
+static float ease_out(float t) {
+  return 1 - expf( -6.0 * t );
 }
 
 static s32 DrawStory( struct TaskData* pTask , AGDrawBuffer* pDBuf ) {
 	int w, h;
 	int a;
 	int x,y;
+  float t;
 
 	if( pTask->Data.story.mode == 0 ) {
 		a = abs( (pTask->Data.story.count%256) - 128 ) + 127;
@@ -57,21 +75,44 @@ static s32 DrawStory( struct TaskData* pTask , AGDrawBuffer* pDBuf ) {
 
 	agDrawSPRITE( pDBuf, 1, x<<2 , y<<2 , (x+w)<<2 , (y+h)<<2 );
 
-	agPictureSetBlendMode( pDBuf , 0 , a , 0 , 0 , 2 , 1 );
-	ageTransferAAC( pDBuf, AG_CG_GAMESTART, 0, &w, &h );
+	//agPictureSetBlendMode( pDBuf , 0 , a , 0 , 0 , 2 , 1 );
+	//ageTransferAAC( pDBuf, AG_CG_GAMESTART, 0, &w, &h );
 
-	x = (1024-w)/2;
-	y = (768/2-h)/2 + 768/2;
+  //x = (1024-w)/2;
+	//y = (768/2-h)/2 + 768/2;
 
-	agDrawSPRITE( pDBuf, 1, x<<2 , y<<2 , (x+w)<<2 , (y+h)<<2 );
+	//agDrawSPRITE( pDBuf, 1, x<<2 , y<<2 , (x+w)<<2 , (y+h)<<2 );
 
-	if( pTask->Data.story.mode == 1 ) {
-		int at = pTask->Data.story.count / 64 * 255 / 80;
+  // message
+  switch( pTask->Data.story.mode ) {
+    case 0:// message: 目を離したらえりたんがいないのだ!
+    case 1:// message: えりたん!今、会いに行くのだ!
+      agPictureSetBlendMode( pDBuf , 0 , 0xff , 0 , 0 , 2 , 1 );
+      ageTransferAAC_RM3( pDBuf, AG_RP_SIN_MESSAGE, 0, &w, &h, pTask->Data.story.mode );
 
-		agPictureSetBlendMode( pDBuf , 0 , at , 0 , 0 , 0 , 1 );
-		agDrawSETFCOLOR( pDBuf , ARGB( 0xff , 0xff , 0xff , 0xff ) );
-		agDrawSPRITE( pDBuf, 0, 0<<2 , 0<<2 , (1024)<<2 , (768)<<2 );
-	};
+      w *= 0.8;
+      h *= 0.8;
+
+      t = (float)min(pTask->Data.story.count, 50) / 50;
+      x = (1024-w)/2;
+      y = (768/2-h)/2 + 768 - 768/2 * ease_out(t);
+      //y = (768/2-h)/2 + 768/2 * ease_out(t);
+
+      agDrawSPRITE( pDBuf, 1, x<<2 , y<<2 , (x+w)<<2 , (y+h)<<2 );
+      break;
+
+    case 2:
+      {
+        int at = pTask->Data.story.count / 64 * 255 / 80;
+
+        agPictureSetBlendMode( pDBuf , 0 , at , 0 , 0 , 0 , 1 );
+        agDrawSETFCOLOR( pDBuf , ARGB( 0xff , 0xff , 0xff , 0xff ) );
+        agDrawSPRITE( pDBuf, 0, 0<<2 , 0<<2 , (1024)<<2 , (768)<<2 );
+      };
+      break;
+  };
+
+
 
 	g_OffsetX++;
 
