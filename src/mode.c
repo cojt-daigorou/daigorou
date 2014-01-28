9240d5e60e9d6ad5c76c34ee766611360d40db8d
Sho Hosoda <gam0022@gmail.com>
2014-01-28 23:07:25 +0900

    retab: mdoe.c

--- src/mode.c
+++ src/mode.c
@@ -9,58 +9,59 @@
 static enum GameMode NowMode;
 
 void InitMode( void ) {
-	NowMode = MODE_NONE;
+  NowMode = MODE_NONE;
 }
 
 void GotoMode( enum GameMode mode ) {
-	struct TaskData* pTask;
+  struct TaskData* pTask;
 
-	ClearAllTask();
+  ClearAllTask();
 
-	switch( mode ) {
-		case MODE_TITLE :
-			g_OffsetX = 0;
-			g_OffsetY = 0;
-			g_PlayerX = 0;
-			g_PlayerY = 0;
+  switch( mode ) {
+    case MODE_TITLE :
+      g_OffsetX = 0;
+      g_OffsetY = 0;
+      g_PlayerX = 0;
+      g_PlayerY = 0;
 
-			g_isGameOver = 0;
+      g_isGameOver = 0;
       g_isStageClear = FALSE;
       g_isGetKeyItem = FALSE;
 
-			g_Score = 0;
-			g_Time = 100;
-			g_Life = 30;
-			g_Star = 30;
+      g_Score = 0;
+      g_Time = 100;
+      g_Life = 30;
+      g_Star = 30;
       g_Stage = 0;
+      //g_Stage = 2;
 
-			pTask = AllocTask();
-			InitTaskBg( pTask , AG_CG_BG_0_0 , 0 );
-			AddlLink( pTask , DISP_LEVEL_BG );
+      pTask = AllocTask();
+      InitTaskBg( pTask , AG_CG_BG_0_0 , 0 );
+      AddlLink( pTask , DISP_LEVEL_BG );
 
-			pTask = AllocTask();
-			InitTaskOpening( pTask );
-			AddlLink( pTask , DISP_LEVEL_OBJECT );
-			break;
+      pTask = AllocTask();
+      InitTaskOpening( pTask );
+      AddlLink( pTask , DISP_LEVEL_OBJECT );
+      break;
 
     case MODE_STORY:
 
-			pTask = AllocTask();
-			InitTaskBg( pTask , AG_CG_BG_0_0 , 0 );
-			AddlLink( pTask , DISP_LEVEL_BG );
+      pTask = AllocTask();
+      InitTaskBg( pTask , AG_CG_BG_0_0 , 0 );
+      AddlLink( pTask , DISP_LEVEL_BG );
 
-			pTask = AllocTask();
-			InitTaskStory( pTask );
-			AddlLink( pTask , DISP_LEVEL_OBJECT );
+      pTask = AllocTask();
+      InitTaskStory( pTask );
+      AddlLink( pTask , DISP_LEVEL_OBJECT );
       break;
 
-		case MODE_GAME :
-			g_OffsetX = 0;
-			g_OffsetY = 0;
-			g_PlayerX = 0;
-			g_PlayerY = 0;
+    case MODE_GAME :
+      g_OffsetX = 0;
+      g_OffsetY = 0;
+      g_PlayerX = 0;
+      g_PlayerY = 0;
 
-			g_isGameOver = 0;
+      g_isGameOver = 0;
       g_isStageClear = FALSE;
       g_isGetKeyItem = FALSE;
 
@@ -86,63 +87,63 @@ void GotoMode( enum GameMode mode ) {
         g_Star = 30;
       }
 
-			pTask = AllocTask();
-			InitTaskBg( pTask , AG_CG_BG_0_0 , 0 );
-			AddlLink( pTask , DISP_LEVEL_BG );
+      pTask = AllocTask();
+      InitTaskBg( pTask , AG_CG_BG_0_0 , 0 );
+      AddlLink( pTask , DISP_LEVEL_BG );
 
-			pTask = AllocTask();
-			InitTaskPlayer( pTask );
-			AddlLink( pTask , DISP_LEVEL_PLAYER );
+      pTask = AllocTask();
+      InitTaskPlayer( pTask );
+      AddlLink( pTask , DISP_LEVEL_PLAYER );
 
-			pTask = AllocTask();
+      pTask = AllocTask();
       InitTaskSenario1( pTask );
-			AddlLink( pTask , DISP_LEVEL_BG );
+      AddlLink( pTask , DISP_LEVEL_BG );
 
       // スコア表示などのインジケーター
 
       // SCORE
-			pTask = AllocTask();
-			InitTaskScore( pTask , -30 , 30 , &(g_Score), 4 );
-			AddlLink( pTask , DISP_LEVEL_SCORE );
+      pTask = AllocTask();
+      InitTaskScore( pTask , -30 , 30 , &(g_Score), 4 );
+      AddlLink( pTask , DISP_LEVEL_SCORE );
 
       // LIFE
-			pTask = AllocTask();
-			InitTaskStatic( pTask , 250 , 10 , AG_CG_ICON_DAIGORO , 1 );
-			AddlLink( pTask , DISP_LEVEL_INFO );
+      pTask = AllocTask();
+      InitTaskStatic( pTask , 250 , 10 , AG_CG_ICON_DAIGORO , 1 );
+      AddlLink( pTask , DISP_LEVEL_INFO );
 
-			pTask = AllocTask();
-			InitTaskStatic( pTask , 335 , 30 , AG_CG_ICON_CROSS , 1 );
-			AddlLink( pTask , DISP_LEVEL_INFO );
+      pTask = AllocTask();
+      InitTaskStatic( pTask , 335 , 30 , AG_CG_ICON_CROSS , 1 );
+      AddlLink( pTask , DISP_LEVEL_INFO );
 
-			pTask = AllocTask();
-			InitTaskScore( pTask , 320 , 30 , &(g_Life), 2 );
-			AddlLink( pTask , DISP_LEVEL_SCORE );
+      pTask = AllocTask();
+      InitTaskScore( pTask , 320 , 30 , &(g_Life), 2 );
+      AddlLink( pTask , DISP_LEVEL_SCORE );
 
       // STAR
-			pTask = AllocTask();
-			InitTaskStatic( pTask , 500 , 18 , AG_CG_ICON_STAR , 1 );
-			AddlLink( pTask , DISP_LEVEL_INFO );
+      pTask = AllocTask();
+      InitTaskStatic( pTask , 500 , 18 , AG_CG_ICON_STAR , 1 );
+      AddlLink( pTask , DISP_LEVEL_INFO );
 
-			pTask = AllocTask();
-			InitTaskStatic( pTask , 575 , 30 , AG_CG_ICON_CROSS , 1 );
-			AddlLink( pTask , DISP_LEVEL_INFO );
+      pTask = AllocTask();
+      InitTaskStatic( pTask , 575 , 30 , AG_CG_ICON_CROSS , 1 );
+      AddlLink( pTask , DISP_LEVEL_INFO );
 
-			pTask = AllocTask();
-			InitTaskScore( pTask , 560 , 30 , &(g_Star), 2 );
-			AddlLink( pTask , DISP_LEVEL_SCORE );
+      pTask = AllocTask();
+      InitTaskScore( pTask , 560 , 30 , &(g_Star), 2 );
+      AddlLink( pTask , DISP_LEVEL_SCORE );
 
       // TIME
-			pTask = AllocTask();
-			InitTaskStatic( pTask , 760 , 10 , AG_CG_INFO_TIME , 1 );
-			AddlLink( pTask , DISP_LEVEL_INFO );
+      pTask = AllocTask();
+      InitTaskStatic( pTask , 760 , 10 , AG_CG_INFO_TIME , 1 );
+      AddlLink( pTask , DISP_LEVEL_INFO );
 
-			pTask = AllocTask();
-			InitTaskScore( pTask , 790 , 30 , &(g_Time), 3 );
-			AddlLink( pTask , DISP_LEVEL_SCORE );
+      pTask = AllocTask();
+      InitTaskScore( pTask , 790 , 30 , &(g_Time), 3 );
+      AddlLink( pTask , DISP_LEVEL_SCORE );
 
-			break;
+      break;
 
-		default :
-			break;
-	};
+    default :
+      break;
+  };
 }
