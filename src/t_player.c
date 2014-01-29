e809d627bbcd6bb5df20b46a4923ea0a683774c3
Yuya Suzuki cojt <s1313088@u.tsukuba.ac.jp>
2014-01-29 14:24:16 +0900

    音楽切り替え用g_Time消費をなくした

--- src/t_player.c
+++ src/t_player.c
@@ -321,7 +321,6 @@ static s32 CalcPlayer( struct TaskData* pTask , u32 Flag ) {
 
     // ‹­UŒ‚(Star Machine Gun) ”­“®ŠJŽn
     if( g_Star >= 10 && ( PadTrg()&PAD_Y ) && !pTask->Data.player.starMachineGun_count ) {
-      g_Time = 35;
       g_Star -= 10;
       pTask->Data.player.starMachineGun_count = 300;
       ageSndMgrPlayOneshot( AS_SND_B04 , 0 , 0x80 , AGE_SNDMGR_PANMODE_LR12 , 0x80 , 0 );
